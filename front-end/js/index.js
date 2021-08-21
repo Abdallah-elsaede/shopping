7f76c8704c46c70c15568f30e22f404536dacb77
Abdallah-elsaede <abdallahmostapha957@gmail.com>
2021-08-21 04:35:52 -0700

    fix auth issue

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,4 +1,8 @@
 window.onload = ()=>{
+    const token = localStorage.getItem('token');
+    if(token == '' || token == null || token == undefined){
+        location.href = 'login.html';
+    }
     getData();
 
 };
